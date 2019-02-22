57a2621cd06b5c014768ea016d70e54d9ff05018
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-02-23 01:00:24 +0530

    Add new code added for comment list component.

--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'dcd-comment-list',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CommentListComponent implements OnInit {
 
   @Input() comments
+  @Output() addComment = new EventEmitter();
 
   constructor() { }
 
