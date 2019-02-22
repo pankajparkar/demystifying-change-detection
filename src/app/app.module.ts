854bb2a6c0a3d8dcec39dfeecebc2ed90b750345
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-02-22 21:11:41 +0530

    Added post-list and post component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
