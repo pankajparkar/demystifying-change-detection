ee1d3ca9c98ef1136636338b03929b3806a13f80
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-02-23 03:41:18 +0530

    Added highlight effect to comment-list component as well.

--- src/app/default-strategy/comment-list/comment-list.component.ts
+++ src/app/default-strategy/comment-list/comment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, NgZone } from '@angular/core';
 
 @Component({
   selector: 'dcd-comment-list',
@@ -10,9 +10,18 @@ export class CommentListComponent implements OnInit {
   @Input() comments
   @Output() addComment = new EventEmitter();
 
-  constructor() { }
+  constructor(private el: ElementRef, private zone: NgZone) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewChecked(): void {
+    this.zone.runOutsideAngular(() => {
+      this.el.nativeElement.classList.add('highlight')
+      setTimeout(() => {
+        this.el.nativeElement.classList.remove('highlight')
+      }, 1500)
+    })
   }
 
 }
